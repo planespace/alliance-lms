3aab4aaf08142456d23b59753c62060741989619
planespace <102373469+planespace@users.noreply.github.com>
2026-07-12 09:18:28 +0000

    hello im doing this agian

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const compression = require("compression");
 // Middleware
 app.use(cors());
 app.use(compression());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 // Serve static files (frontend)
 app.use(express.static(path.join(__dirname, "public")));
